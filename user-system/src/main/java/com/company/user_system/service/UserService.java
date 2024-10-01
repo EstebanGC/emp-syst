4175d4794f0e17f8fa0803f7b72a9d5113a22deb
EstebanGC <johan.gal.car@gmail.com>
2024-10-01 18:20:08 -0500

    Adding Service

--- user-system/src/main/java/com/company/user_system/service/UserService.java
+++ user-system/src/main/java/com/company/user_system/service/UserService.java
@@ -1,7 +1,22 @@
 package com.company.user_system.service;
 
-public class UserService {
+import com.company.user_system.dto.UserDto;
+import com.company.user_system.entity.User;
 
-//    void saveUser(UserDto userDto);
+import java.util.List;
 
+public interface UserService {
+    void saveUser(UserDto userDto);
+
+    List<UserDto> findAllUsers();
+
+    User findUserByEmail(String email);
+
+    UserDto findUserById(Long userId);
+
+    boolean doesUserExists(Long userId);
+
+    void editUser(UserDto updatedUserDto, Long userId);
+
+    void deleteUserById(Long userId);
 }
