a00a3f896b6b2c6c10781e4729863ed7cdb5e780
EstebanGC <johan.gal.car@gmail.com>
2024-10-27 22:57:47 -0500

    Fixing conflicts

--- user-system/src/main/java/com/company/user_system/repository/RoleRepository.java
+++ user-system/src/main/java/com/company/user_system/repository/RoleRepository.java
@@ -1,11 +1,10 @@
 package com.company.user_system.repository;
 
 import com.company.user_system.entity.Role;
-import com.company.user_system.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<User, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByUsername(String username);
 }
--- user-system/src/main/java/com/company/user_system/repository/UserRepository.java
+++ user-system/src/main/java/com/company/user_system/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.company.user_system.repository;
 
-import com.company.user_system.entity.Role;
+
 import com.company.user_system.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Role findByEmail(String username);
+    Optional<User> findByEmail(String username);
 }
