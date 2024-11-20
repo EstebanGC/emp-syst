d0b16575721bc7f05808299e5ea3cd83072d5d9b
EstebanGC <johan.gal.car@gmail.com>
2024-11-19 20:58:28 -0500

    Setting UserDetails

--- user-system/src/main/java/com/company/user_system/security/CustomUserDetailsService.java
+++ user-system/src/main/java/com/company/user_system/security/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.company.user_system.security;
 
+import com.company.user_system.entity.Role;
 import com.company.user_system.entity.User;
 import com.company.user_system.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,8 +10,10 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -23,9 +26,21 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Invalid username or password"));
+        User user = userRepository.findByEmail(email);
 
-        return int 1;
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                    mapRolesToAuthorities(user.getRoleId()));
+        } else {
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+    }
+
+    private Collection< ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
+        Collection < ? extends GrantedAuthority> mapRoles = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        return mapRoles;
     }
 }
