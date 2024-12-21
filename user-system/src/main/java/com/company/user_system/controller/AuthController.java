5f5bfcff60f48a0f5385b0f0e63d8c1bfd5422f2
EstebanGC <johan.gal.car@gmail.com>
2024-12-20 23:28:35 -0500

    Setting AuthController

--- user-system/src/main/java/com/company/user_system/controller/AuthController.java
+++ user-system/src/main/java/com/company/user_system/controller/AuthController.java
@@ -1,10 +1,18 @@
 package com.company.user_system.controller;
 
 import com.company.user_system.repository.UserRepository;
+import com.company.user_system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    public UserService userService;
+
+
+
 
 }
