9eb8732ec29af852b8822e79abf028b638722a0c
EstebanGC <johan.gal.car@gmail.com>
2025-01-02 03:39:26 -0500

    Adding AuthController

--- user-system/src/main/java/com/company/user_system/controller/AuthController.java
+++ user-system/src/main/java/com/company/user_system/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.company.user_system.controller;
 
-import com.company.user_system.repository.UserRepository;
+import com.company.user_system.dto.UserDto;
+import com.company.user_system.entity.User;
 import com.company.user_system.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class AuthController {
@@ -12,8 +16,26 @@ public class AuthController {
     @Autowired
     public UserService userService;
 
-    public void whatever() {
-        System.out.println("Whatever");
+    public AuthController(UserService userService){
+        this.userService = userService;
     }
-    
+
+    public String singUp(
+            @Valid @ModelAttribute("user") UserDto userDto,
+            BindingResult result,
+            Model model) {
+
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+
+        }
+        userService.saveUser(userDto);
+        return "redirect:/register?success=true";
+    }
+
 }
+
+
